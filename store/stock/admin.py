e7a855c3f8f12811df21d8bfd2b9e5b345d7a6de
marteszibellina <valerababahov@yandex.ru>
2025-05-27 15:43:09 +0300

    Created apps, models, admin zone

---
+++ store/stock/admin.py
@@ -0,0 +1,82 @@
+"""Admin for the Stock"""
+
+from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Group
+
+from stock.models import (Category, Product,
+                          ProductImage, ShoppingCart, SubCategory)
+
+User = get_user_model()
+
+
+class SubCategoryInline(admin.TabularInline):
+    """SubCategory inline."""
+    model = SubCategory
+    fields = ('name', 'slug', 'image')
+    extra = 1
+
+
+@admin.register(SubCategory)
+class SubCategoryAdmin(admin.ModelAdmin):
+    """SubCategory admin."""
+    list_display = ('name', 'slug', 'image', 'category')
+    search_fields = ('name', 'slug')
+    list_display_links = ('name', 'category')
+    prepopulated_fields = {'slug': ('name',)}
+    empty_value_display = '-empty-'
+
+
+class ProductImageInline(admin.TabularInline):
+    """ProductImage inline."""
+    model = ProductImage
+    fields = ('image', 'order')
+    extra = 1
+
+
+class ProductInline(admin.TabularInline):
+    """Product inline."""
+    model = Product
+    fields = ('name', 'slug', 'category', 'subcategory', 'price',)
+    extra = 1
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    """Product admin."""
+    list_display = ('name', 'slug', 'category', 'subcategory', 'price')
+    search_fields = ('name', 'slug')
+    list_display_links = ('name', 'category')
+    prepopulated_fields = {'slug': ('name',)}
+    empty_value_display = '-empty-'
+    inlines = (ProductImageInline, )
+
+
+@admin.register(ProductImage)
+class ProductImageAdmin(admin.ModelAdmin):
+    """ProductImage admin."""
+    list_display = ('image', 'order', 'product')
+    search_fields = ('image', 'order', 'product')
+    empty_value_display = '-empty-'
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    """Category admin."""
+    list_display = ('name', 'slug', 'image',)
+    list_display_links = ('name',)
+    search_fields = ('name', 'slug')
+    prepopulated_fields = {'slug': ('name',)}
+    empty_value_display = '-empty-'
+    inlines = (SubCategoryInline, ProductInline)
+
+
+@admin.register(ShoppingCart)
+class ShoppingCartAdmin(admin.ModelAdmin):
+    """ShoppingCart admin."""
+    list_display = ('user', 'product', 'quantity', 'price')
+    search_fields = ('user', 'product', 'quantity', 'price')
+    empty_value_display = '-empty-'
+
+
+admin.site.unregister(Group)
---
+++ store/stock/models.py
@@ -0,0 +1,107 @@
+"""Stock models."""
+
+from django.contrib.auth import get_user_model
+from django.db import models
+
+User = get_user_model()
+
+from stock.constants import (
+    CATEGORY_NAME_LENGTH, CATEGORY_SLUG_LENGTH,
+    PRODUCT_NAME_LENGTH, PRODUCT_SLUG_LENGTH,
+    PRODUCT_PRICE_MAX_DIGITS, PRODUCT_PRICE_DECIMAL_PLACES,
+    TEXT_SLICE,)
+
+class Category(models.Model):
+    """Category model."""
+
+    name = models.CharField(max_length=CATEGORY_NAME_LENGTH)
+    slug = models.SlugField(max_length=CATEGORY_SLUG_LENGTH,
+                            unique=True)
+    image = models.ImageField(upload_to='category/images',
+                              blank=True)
+
+    def __str__(self):
+        """Return category name."""
+        return self.name[:TEXT_SLICE]
+
+    class Meta:
+        """Meta-Class for Category."""
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+        ordering = ['name']
+
+
+class SubCategory(models.Model):
+    """SubCategory model."""
+
+    name = models.CharField(max_length=CATEGORY_NAME_LENGTH)
+    slug = models.SlugField(max_length=CATEGORY_SLUG_LENGTH,
+                            unique=True)
+    image = models.ImageField(upload_to='subcategory/images',)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        """Return subcategory name."""
+        return f'{self.category.name}[{TEXT_SLICE}]:{self.name}[:{TEXT_SLICE}]'
+
+    class Meta:
+        """Meta-Class for SubCategory."""
+        verbose_name = 'SubCategory'
+        verbose_name_plural = 'SubCategories'
+        ordering = ['name']
+
+
+class ProductImage(models.Model):
+    """ProductImage model."""
+
+    image = models.ImageField('product-images',
+                              upload_to='product/images')
+    order = models.PositiveSmallIntegerField(default=0,
+                                             verbose_name='Order')
+    product = models.ForeignKey('Product', on_delete=models.CASCADE)
+
+    def __str__(self):
+        """Return product name."""
+        return f'{self.product.name}: Order {self.order}'
+
+    class Meta:
+        """Meta-Class for ProductImage."""
+        verbose_name = 'ProductImage'
+        verbose_name_plural = 'ProductImages'
+        ordering = ['order']
+
+
+class Product(models.Model):
+    """Product model."""
+
+    name = models.CharField(max_length=PRODUCT_NAME_LENGTH)
+    slug = models.SlugField(max_length=PRODUCT_SLUG_LENGTH,
+                            unique=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    subcategory = models.ForeignKey(SubCategory, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=PRODUCT_PRICE_MAX_DIGITS,
+                                decimal_places=PRODUCT_PRICE_DECIMAL_PLACES)
+
+    def __str__(self):
+        """Return product name."""
+        return f'{self.name}:{self.price} of {self.category.name}:{self.subcategory.name}'
+
+    class Meta:
+        """Meta-Class for Product."""
+        verbose_name = 'Product'
+        verbose_name_plural = 'Products'
+        ordering = ['name']
+
+
+class ShoppingCart(models.Model):
+    """ShoppingCart model."""
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveSmallIntegerField(default=1)
+    price = models.DecimalField(max_digits=PRODUCT_PRICE_MAX_DIGITS,
+                                decimal_places=PRODUCT_PRICE_DECIMAL_PLACES)
+
+    def __str__(self):
+        """Return product name."""
+        return f'{self.user.username}:{self.product.name}:{self.quantity}:{self.price}'
